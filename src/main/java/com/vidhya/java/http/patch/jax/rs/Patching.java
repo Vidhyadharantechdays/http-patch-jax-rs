9934d283c8393bc70dbba083326480f782405600
Vidhyadharan D <it.vidhyadharan@gmail.com>
2016-03-08 17:36:35 +0530

    Clean up removed the unwanted comment

--- src/main/java/com/vidhya/java/http/patch/jax/rs/Patching.java
+++ src/main/java/com/vidhya/java/http/patch/jax/rs/Patching.java
@@ -68,17 +68,6 @@ public class Patching implements ReaderInterceptor {
     private  Providers providers;
 
     
-    /**
-     * {@code PatchingInterceptor} injection constructor.
-     *
-     * @param uriInfo {@code javax.ws.rs.core.UriInfo} proxy instance.
-     * @param workers {@link org.glassfish.jersey.message.MessageBodyWorkers} message body workers.
-     */
-//    public Patching(@Context UriInfo uriInfo, @Context MessageBodyWorkers workers) {
-//        this.uriInfo = uriInfo;
-//        this.workers = workers;
-//    }
-
     @SuppressWarnings("unchecked")
     @Override
     public Object aroundReadFrom(ReaderInterceptorContext readerInterceptorContext) throws IOException, WebApplicationException {
